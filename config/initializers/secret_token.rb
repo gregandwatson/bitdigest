f1dc9e3624b23154724ff27490d5ff01485bc824
Wavell Watson <wavell.watson@gmail.com>
2012-08-01 01:31:10 -0500

    browsercms with page installed at localhost 3000

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bitdigest::Application.config.secret_token = '3083f6de7cdce76b46dec52c039552e271f67f471a2f74756748bc9f1622a0d1743abad1af00d9de7e2e937567fc2f7fea9a03a55e876c309e899e38b0f318d0'
